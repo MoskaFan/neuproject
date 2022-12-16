0ce4fd572d630553cfc15005b93ea9a7e3fa81d3
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-16 11:35:08 +0100

    Add changes modell location

--- backend/src/main/java/de/neuefische/backend/modelle/Location.java
+++ backend/src/main/java/de/neuefische/backend/modelle/Location.java
@@ -1,9 +1,12 @@
+package de.neuefische.backend.modelle;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 
 @Data
@@ -13,9 +16,17 @@ import java.math.BigDecimal;
 public class Location {
 
     private String id;
+    private String name;
+    private String image;
+    private String description;
+    private String website;
+    private BigDecimal pricePerPerson;
     private double size;
+    private String eventType;
     private int maxCapacity;
-    private BigDecimal price;
     private Address address;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
 
 }
