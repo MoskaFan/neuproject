c3664e434e928b15a4aec38d2c598863b2028d0f
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-29 21:02:27 +0100

    Fix problems from review

--- frontend/src/AddLocation.tsx
+++ frontend/src/AddLocation.tsx
@@ -1,9 +1,7 @@
-
 import React, {ChangeEvent, FormEvent, useState} from "react";
 import {Button} from "@mui/material";
-import { LocationData} from "./entity/LocationData";
+import {LocationData} from "./entity/LocationData";
 import './SignUp.css'
-
 
 
 type AddLocationProps = {
@@ -14,41 +12,30 @@ export default function AddLocation(props: AddLocationProps) {
 
     const emptyInput: LocationData = {
 
-        name:"",
-        image:"",
-        description:"",
-        website:"",
-        pricePerPerson:"",
+        name: "",
+        image: "",
+        description: "",
+        website: "",
+        pricePerPerson: "",
         size: "",
-        eventType:"",
+        eventType: "",
         maxCapacity: "",
 
     }
 
-
     const [location, setLocation] = useState(emptyInput);
 
-
-
-
-
-
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
         e.preventDefault();
         const {name, value} = e.target;
         setLocation((prev) => ({...prev, [name]: value}));
     }
 
-    const handleSubmit = (event: FormEvent) => {
+    function handleSubmit(event: FormEvent) {
         event.preventDefault()
-
-            props.addLocation(location)
-            setLocation(emptyInput)
-        }
-
-
-
+        props.addLocation(location)
+        setLocation(emptyInput)
+    }
 
 
     return (
@@ -59,24 +46,24 @@ export default function AddLocation(props: AddLocationProps) {
                 </div>
                 <div className="form">
                     <label htmlFor="name">Bezeichnung von Location: </label>
-                    <input type="text" name="name" value={location.name} onChange={handleChange} />
+                    <input type="text" name="name" value={location.name} onChange={handleChange}/>
 
                     <label htmlFor="image">Image von Location: </label>
-                    <input type="text" name="image" value={location.image} onChange={handleChange} />
+                    <input type="text" name="image" value={location.image} onChange={handleChange}/>
 
                     <label htmlFor="website">Website von Location: </label>
-                    <input type="text" name="website" value={location.website} onChange={handleChange} />
-                <label htmlFor="description">Beschreibung von Location: </label>
-                <input type="area" name="description" value={location.description} onChange={handleChange} />
-                <label htmlFor="pricePerPerson">Preis prop Person: </label>
-                <input type="number" name="pricePerPerson" value={location.pricePerPerson} onChange={handleChange} />
-                <label htmlFor="size">Fläche: </label>
-                <input type="number" name="size" value={location.size} onChange={handleChange} />
-                <label htmlFor="eventType">Anlass: </label>
-                <input type="text" name="eventType" value={location.eventType} onChange={handleChange} />
-                <label htmlFor="maxCapacity">Maximale Kapazität: </label>
-                <input type="number" name="maxCapacity" value={location.maxCapacity} onChange={handleChange} />
-                    <Button type="submit" className = {"btn"}>Speichern</Button>
+                    <input type="text" name="website" value={location.website} onChange={handleChange}/>
+                    <label htmlFor="description">Beschreibung von Location: </label>
+                    <input type="area" name="description" value={location.description} onChange={handleChange}/>
+                    <label htmlFor="pricePerPerson">Preis prop Person: </label>
+                    <input type="number" name="pricePerPerson" value={location.pricePerPerson} onChange={handleChange}/>
+                    <label htmlFor="size">Fläche: </label>
+                    <input type="number" name="size" value={location.size} onChange={handleChange}/>
+                    <label htmlFor="eventType">Anlass: </label>
+                    <input type="text" name="eventType" value={location.eventType} onChange={handleChange}/>
+                    <label htmlFor="maxCapacity">Maximale Kapazität: </label>
+                    <input type="number" name="maxCapacity" value={location.maxCapacity} onChange={handleChange}/>
+                    <Button type="submit" className={"btn"}>Speichern</Button>
 
                 </div>
 
