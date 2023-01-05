c850e6ff7e363841ab29c20008485d423469000f
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-05 12:13:55 +0100

    Fix bug with AddLocation.tsx

--- backend/src/main/java/de/neuefische/backend/modelle/Location.java
+++ backend/src/main/java/de/neuefische/backend/modelle/Location.java
@@ -1,33 +1,18 @@
 package de.neuefische.backend.modelle;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Document("locations")
-
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
-    public Location(String id, String name, String image, String description,
-                    String website, BigDecimal pricePerPerson, String size,
-                    String eventType, String maxCapacity, Address address,
-                    LocalDate startDate, LocalDate endDate) {
-        this.id = id;
-        this.name = name;
-        this.image = image;
-        this.description = description;
-        this.website = website;
-        this.pricePerPerson = pricePerPerson;
-        this.size = size;
-        this.eventType = eventType;
-        this.maxCapacity = maxCapacity;
-        this.address = address;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
+
 
     @Id
     private String id;
@@ -36,112 +21,12 @@ public class Location {
     private String description;
     private String website;
     private BigDecimal pricePerPerson;
-    private String size;
+    private int size;
     private String eventType;
-    private String maxCapacity;
+    private int maxCapacity;
     private Address address;
-    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate startDate;
-    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate endDate;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public BigDecimal getPricePerPerson() {
-        return pricePerPerson;
-    }
-
-    public void setPricePerPerson(BigDecimal pricePerPerson) {
-        this.pricePerPerson = pricePerPerson;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getEventType() {
-        return eventType;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public String getMaxCapacity() {
-        return maxCapacity;
-    }
-
-    public void setMaxCapacity(String maxCapacity) {
-        this.maxCapacity = maxCapacity;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = LocalDate.parse(startDate);
-    }
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = LocalDate.parse(endDate);
-    }
-
-
 
 
 }
