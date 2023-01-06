35bc26f424cd3b271523434a553c622ea1dcb75a
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-06 09:17:55 +0100

    Change class to record by LocationDTO

--- backend/src/main/java/de/neuefische/backend/modelle/LocationDTO.java
+++ backend/src/main/java/de/neuefische/backend/modelle/LocationDTO.java
@@ -1,25 +1,19 @@
 package de.neuefische.backend.modelle;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class LocationDTO {
 
-    private String name;
-    private String image;
-    private String description;
-    private String website;
-    private BigDecimal pricePerPerson;
-    private int size;
-    private String eventType;
-    private int maxCapacity;
-    private Address address;
-    private LocalDate startDate;
-    private LocalDate endDate;
+public record LocationDTO (
 
+     String name,
+     String image,
+     String description,
+     String website,
+     BigDecimal pricePerPerson,
+     int size,
+     String eventType,
+     int maxCapacity,
+     Address address,
+     LocalDate startDate,
+     LocalDate endDate)
+    {
 }
