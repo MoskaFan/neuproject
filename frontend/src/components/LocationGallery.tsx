7581ca8d7d003a8632960db7f91230645f8650ea
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-11 13:58:31 +0100

    Change search bar

--- frontend/src/components/LocationGallery.tsx
+++ frontend/src/components/LocationGallery.tsx
@@ -1,6 +1,7 @@
 import {LocationData} from "../entity/locationData";
 import LocationCard from "./LocationCard";
 import {ChangeEvent, useState} from "react";
+import {Button} from "@mui/material";
 
 
 type LocationGalleryProps = {
@@ -10,53 +11,80 @@ type LocationGalleryProps = {
 export default function LocationGallery(props: LocationGalleryProps){
 
     const [searchCity, setSearchCity] = useState("")
-    const [minPrice, setMinPrice] = useState(0)
-    const [maxPrice, setMaxPrice] = useState(10000)
+    const [minPrice, setMinPrice] = useState<number>(0)
+    const [maxPrice, setMaxPrice] = useState<number>(10000)
     const [searchSize, setSearchSize] = useState("")
     const [searchCapacity, setSearchCapacity] = useState("")
     const [searchEventType, setSearchEventType] = useState("")
+    const [filteredLocations, setFilteredLocations] = useState<LocationData[]>(props.locations)
 
-    const filteredCity = props.locations.filter((location) =>
-        location.address.city.toLowerCase().includes(searchCity.toLowerCase()))
-    function handleCityChange(event:ChangeEvent<HTMLInputElement>) {
-        setSearchCity(searchCity)
+    function filterListByCity(){
+        const filteredListByCity = filteredLocations.filter((location) =>
+            location.address.city.toLowerCase().includes(searchCity.toLowerCase()))
+        setFilteredLocations(filteredListByCity)
     }
+    function filterListByEventType(){
+        const filteredListByEventType = filteredLocations.filter((location) =>
+            location.eventType?.toLowerCase().includes(searchEventType.toLowerCase()))
+        setFilteredLocations(filteredListByEventType)
+    }
+    function filterListByCapacity(event:ChangeEvent<HTMLInputElement>){
+        const value = event.target.value
+            if(value === "1"){
+                setFilteredLocations(filteredLocations.filter((location) =>
+                    location.maxCapacity! < 50))
+            }else if(value === "2"){
+            setFilteredLocations(filteredLocations.filter((location) =>
+                location.maxCapacity! > 50 && location.maxCapacity!<100))
+            }else{
+                setFilteredLocations(filteredLocations.filter((location) =>
+                    location.maxCapacity!>100))
+            }
+
+    }
+    function filterListByPrice(event:ChangeEvent<HTMLInputElement>){
+
+            setFilteredLocations(filteredLocations.filter((location) =>
+                location.pricePerPerson! > minPrice && location.pricePerPerson! < maxPrice))
+
+
+    }
+    function filterListBySize(event:ChangeEvent<HTMLInputElement>){
+        const value = event.target.value
+        if(value === "1"){
+            setFilteredLocations(filteredLocations.filter((location) =>
+                location.size! < 100))
+        }else if(value === "2"){
+            setFilteredLocations(filteredLocations.filter((location) =>
+                location.size! > 100 && location.size! < 200))
+        }else{
+            setFilteredLocations(filteredLocations.filter((location) =>
+                location.size!>200))
+        }
+
+    }
+
+    function handleCityChange(event:ChangeEvent<HTMLInputElement>) {
+        setSearchCity(event.target.value)
+    }
+
     function handleEventType(event:ChangeEvent<HTMLInputElement>) {
-        setSearchEventType(searchEventType)
+        setSearchEventType(event.target.value)
     }
 
     function handleMaxPriceValue(event:ChangeEvent<HTMLInputElement>) {
-        setMaxPrice(maxPrice)
-
+        setMaxPrice(parseInt(event.target.value))
     }
     function handleMinPriceValue(event:ChangeEvent<HTMLInputElement>) {
-        setMinPrice(minPrice)
-
+        setMinPrice(parseInt(event.target.value))
     }
 
     function handleCapacity(event:ChangeEvent<HTMLInputElement>) {
-        setSearchCapacity(searchCapacity)
+        setSearchCapacity(event.target.value)
     }
     function handleSize(event:ChangeEvent<HTMLInputElement>) {
-        setSearchSize(searchSize)
+        setSearchSize(event.target.value)
     }
-    // const filteredPrice = props.locations.filter((location:LocationData,
-    //                                               ) =>
-
-        // if(searchPrice===0 || searchPrice===10000){
-        //     return 0<location.price<10000;
-        // }else{
-        //     return 0<location.price<searchPrice;
-        // }
-        //     location.pricePerPerson?.toString(searchPrice))
- /*   const filteredSize = props.locations.filter((location) =>
-        location.size ?
-    const filteredCapacity = props.locations.filter((location) =>
-        location.capacity?.toLowerCase().includes(searchCity.toLowerCase()))
-    const filteredEventType = props.locations.filter((location) =>
-        location.eventType?.toLowerCase().includes(searchEventType.toLowerCase()))*/
-
-
 
     const locationComponent = props.locations.map((location: LocationData) => {
         return <LocationCard location={location} key={location.id}/>
@@ -64,93 +92,97 @@ export default function LocationGallery(props: LocationGalleryProps){
 
     return(
         <div className={"card-gallery"}>
-            <label htmlFor="City">Stadt:</label>
-            <input name="City" value={searchCity} onChange = {handleCityChange}/>
-            <fieldset className = {"price-input"}>
-                <h3>Preis:</h3>
-                <div>
+            <form>
+                <label htmlFor="City">Stadt:</label>
+                <input name="City" value={searchCity} onChange = {handleCityChange}/>
+                <fieldset className = {"price-input"}>
+                    <h3>Preis:</h3>
                     <div>
-                        <label htmlFor="Min">Min:</label>
-                        <input type="number" className="Min" name="Min"
-                             value={minPrice} onChange={handleMinPriceValue}/>
+                        <div>
+                            <label htmlFor="Min">Min:</label>
+                            <input type="number" className="Min" name="minPrice"
+                                   value={minPrice} onChange={handleMinPriceValue}/>
 
+                        </div>
+                        <div>-</div>
+                        <div>
+                            <label htmlFor="Max">Max:</label>
+                            <input type="number" className="Max" name="maxPrice"
+                                   value={maxPrice} onChange={handleMaxPriceValue}/>
+
+                        </div>
                     </div>
-                    <div>-</div>
+                </fieldset>
+                <fieldset>
+                    <h3>Fläche:</h3>
+
                     <div>
-                        <label htmlFor="Max">Max:</label>
-                        <input type="number" className="Max" name="Max"
-                                value={maxPrice} onChange={handleMaxPriceValue}/>
-
-                    </div>
-                </div>
-            </fieldset>
-            <fieldset>
-                <h3>Fläche:</h3>
-
-                <div>
-                    <input type="checkbox" id="hundert" name="hundert"
-                           onChange = {handleSize}/>
+                        <input type="checkbox" id="hundert" name="hundert"
+                               onChange = {handleSize} value = {1}/>
                         <label htmlFor="hundert">0-100 m2</label>
-                </div>
+                    </div>
 
-                <div>
-                    <input type="checkbox" id="zweihundert" name="zweihundert"
-                           onChange = {handleSize}/>
+                    <div>
+                        <input type="checkbox" id="zweihundert" name="zweihundert"
+                               onChange = {handleSize} value = {2}/>
                         <label htmlFor="zweihundert">100-200 m2</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="überzweihundert" name="überzweihundert"
-                           onChange = {handleSize}/>
-                    <label htmlFor="überzweihundert">über 200 m2</label>
-                </div>
-            </fieldset>
-            <fieldset>
-                <h3>Anlass:</h3>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="überzweihundert" name="überzweihundert"
+                               onChange = {handleSize} value = {3}/>
+                        <label htmlFor="überzweihundert">über 200 m2</label>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <h3>Anlass:</h3>
 
-                <div>
-                    <input type="checkbox" id="hochzeit" name="hochzeit"
-                           onChange = {handleEventType} />
+                    <div>
+                        <input type="checkbox" id="hochzeit" name="hochzeit"
+                               onChange = {handleEventType} />
                         <label htmlFor="hochzeit">Hochzeit</label>
-                </div>
+                    </div>
 
-                <div>
-                    <input type="checkbox" id="party" name="party"
-                           onChange = {handleEventType}/>
-                    <label htmlFor="party">Party</label>
-                </div>
+                    <div>
+                        <input type="checkbox" id="party" name="party"
+                               onChange = {handleEventType}/>
+                        <label htmlFor="party">Party</label>
+                    </div>
 
-                <div>
-                    <input type="checkbox" id="tagung" name="tagung"
-                           onChange = {handleEventType}/>
-                    <label htmlFor="tagung">Tagung</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="konferenz" name="konferenz"
-                           onChange = {handleEventType}/>
-                    <label htmlFor="konferenz">Konferenz</label>
-                </div>
-            </fieldset>
-            <fieldset>
-                <h3>Personenanzahl:</h3>
+                    <div>
+                        <input type="checkbox" id="tagung" name="tagung"
+                               onChange = {handleEventType}/>
+                        <label htmlFor="tagung">Tagung</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="konferenz" name="konferenz"
+                               onChange = {handleEventType}/>
+                        <label htmlFor="konferenz">Konferenz</label>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <h3>Personenanzahl:</h3>
 
 
-                <div>
-                    <input type="checkbox" id="zehn" name="zehn"
-                           onChange = {handleCapacity}/>
-                    <label htmlFor="zehn">0-50 Personen</label>
-                </div>
+                    <div>
+                        <input type="checkbox" id="zehn" name="zehn" value = {1}
+                               onChange = {handleCapacity}/>
+                        <label htmlFor="zehn">0-50 Personen</label>
+                    </div>
 
-                <div>
-                    <input type="checkbox" id="hundert" name="hundert"
-                           onChange = {handleCapacity}/>
-                    <label htmlFor="hundert">50-100 Personen</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="übereinhundert" name="übereinhundert"
-                           onChange = {handleCapacity}/>
-                    <label htmlFor="übereinhundert">über 100 Personen</label>
-                </div>
-            </fieldset>
+                    <div>
+                        <input type="checkbox" id="hundert" name="hundert"
+                               onChange = {handleCapacity} value = {2}/>
+                        <label htmlFor="hundert">50-100 Personen</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="übereinhundert" name="übereinhundert"
+                               onChange = {handleCapacity} value = {3}/>
+                        <label htmlFor="übereinhundert">über 100 Personen</label>
+                    </div>
+                </fieldset>
+                <Button>Suchen</Button>
+            </form>
+
             {locationComponent}
 
 
