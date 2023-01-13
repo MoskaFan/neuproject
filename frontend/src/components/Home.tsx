3f8c4670b0939c3b5daebe6780c9c38790cb4f56
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-14 00:23:55 +0100

    Change home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,25 @@
+import {Button, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import "../styles/Home.css"
+
 export default function Home() {
+
+    const navigate = useNavigate();
+
     return(
-    <div>
-        <h1>Home</h1>
-    </div>
+        <section className={"home-page"}>
+            <Typography variant={"h1"}>
+
+            Perfekte Locations finden
+
+        </Typography>
+            <br/>
+            <img alt = "start" src = "https://www.eventano.com/app/uploads/2022/02/loft14-bar-berlin-philosophie-slider-1680x600.jpg" />
+            <br/>
+            <br/>
+            <Button onClick={() => navigate("/locations")} className={"btn"}>Perfekte Location finden</Button>
+        </section>
+
+
     )
 }
