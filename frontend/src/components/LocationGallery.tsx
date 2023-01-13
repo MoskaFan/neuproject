d70af855b36e915886ed54450bbca0d07f0cfe6a
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-13 12:32:28 +0100

    Remove SearchBar

--- frontend/src/components/LocationGallery.tsx
+++ frontend/src/components/LocationGallery.tsx
@@ -1,7 +1,7 @@
 import {LocationData} from "../entity/locationData";
 import LocationCard from "./LocationCard";
-import {useEffect, useState} from "react";
-import SearchBar from "./SearchBar";
+import {ChangeEvent, useState} from "react";
+import {Button} from "@mui/material";
 
 
 type LocationGalleryProps = {
@@ -13,80 +13,122 @@ export default function LocationGallery(props: LocationGalleryProps) {
 
 
     const [searchCity, setSearchCity] = useState<string>("")
-    const [minPrice, setMinPrice] = useState<number>(0)
-    const [maxPrice, setMaxPrice] = useState<number>(10000)
-    const [searchSize, setSize] = useState<number>(0)
-    const [searchCapacity, setCapacity] = useState<number>(0)
+    const [minPrice, setMinPrice] = useState<string>("")
+    const [maxPrice, setMaxPrice] = useState<string>("")
+    const [searchSize, setSize] = useState<string>("")
+    const [searchCapacity, setCapacity] = useState<string>("")
     const [searchEventType, setSearchEventType] = useState<string>("")
-    const [searchResults, setSearchResults] = useState<LocationData[]>([])
 
 
+    const filterByCity = props.locations.filter((location) => {
+        return searchCity !== "" && location.address!.city.includes(searchCity)
+    })
 
-    useEffect(() => {
-        const results = props.locations
-            .filter((location) =>
-                location.address!.city.toLowerCase().includes(searchCity.toLowerCase()))
-            .filter((location) => location.pricePerPerson! > minPrice
-                && location.pricePerPerson! < maxPrice)
-            .filter((location) => location.size! <= searchSize)
-            .filter((location) => location.maxCapacity! <= searchCapacity)
-            .filter((location) => location.eventType!
-                .toLowerCase().includes(searchEventType.toLowerCase()))
-        setSearchResults(results);
-    }, [])
+    const filterByPrice = props.locations.filter((location) => {
+        return minPrice !== "" && maxPrice !== "" && location.pricePerPerson! > parseInt(minPrice)
+            && location.pricePerPerson! < parseInt(maxPrice)
+    })
+    const filterByCapacity = props.locations.filter((location) => {
+        return searchCapacity !== "" && location.maxCapacity! <= parseInt(searchCapacity)
+    })
 
-    const locationComponent =
-        searchResults.map((location: LocationData) => {
+    const filterBySize= props.locations.filter((location) => {
+        return searchSize!== "" && location.size! <= parseInt(searchCapacity)
+    })
+
+    const filterByEventType = props.locations.filter((location) => {
+        return searchEventType !== "" && location.eventType!
+            .toLowerCase().includes(searchEventType.toLowerCase())
+    })
+
+
+    const locationCardsCity =
+        filterByCity.map((location: LocationData) => {
             return <LocationCard location={location} key={location.id}/>
         })
 
-
-    function handleCityChange(searchCity: string) {
-        setSearchCity(searchCity)
-    }
-
-    function handleEventType(searchEventType: string) {
-        setSearchEventType(searchEventType)
-    }
-
-    function handleMaxPriceValue(maxPrice: number) {
-        setMaxPrice(maxPrice)
-    }
-
-    function handleMinPriceValue(minPrice: number){
-        setMinPrice(minPrice)
-    }
-
-    function handleCapacity(searchCapacity: number) {
-        setCapacity(searchCapacity)
-    }
-
-    function handleSize(searchSize: number) {
-        setSize(searchSize)
-    }
-    function handleSubmit(searchResults: LocationData[]){
-        setSearchResults(searchResults)
-    }
-
-    function mapLocations() {
-        return searchResults.length > 0 ? locationComponent : <p>Keine Angaben</p>
-    }
+    const locationCardsSize =
+        filterBySize.map((location: LocationData) => {
+            return <LocationCard location={location} key={location.id}/>
+        })
+    const locationCardsPrice =
+        filterByPrice.map((location: LocationData) => {
+            return <LocationCard location={location} key={location.id}/>
+        })
+    const locationCardsCapacity =
+        filterByCapacity.map((location: LocationData) => {
+            return <LocationCard location={location} key={location.id}/>
+        })
+    const locationCardsEventType =
+        filterByEventType.map((location: LocationData) => {
+            return <LocationCard location={location} key={location.id}/>
+        })
 
     return (
         <section>
             <section>
-                <SearchBar searchCity={handleCityChange}
-                           searchCapacity={handleCapacity}
-                           searchMinPrice={handleMinPriceValue}
-                           searchMaxPrice={handleMaxPriceValue}
-                           searchSize={handleSize}
-                           searchEventType={handleEventType}
-                           handleSubmit={handleSubmit}/>
+                <form>
+                    <fieldset>
+                        <h3>Stadt:</h3>
+                        <label htmlFor="City">Stadt:</label>
+                        <input name="City" onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                            setSearchCity(event.target.value)}/>
+                    </fieldset>
+                    <fieldset className={"price-input"}>
+                        <h3>Preis</h3>
+                        <div>
+                            <label htmlFor="Min">Min:</label>
+                            <input type="number" className="minPrice" name="minPrice"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                       setMinPrice(event.target.value)}/>
+                        </div>
+                        <div>-</div>
+                        <div>
+                            <label htmlFor="Max">Max:</label>
+                            <input type="number" className="maxPrice" name="maxPrice"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                       setMaxPrice(event.target.value)}/>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <h3>Fläche:</h3>
+                        <div>
+                            <label htmlFor="hundert">Max. Fläche: </label>
+                            <input type="number" className={"size"} name="size"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                       setSize(event.target.value)}/>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <h3>Anlass:</h3>
+                        <div>
+                            <label htmlFor="anlass">Anlass: </label>
+                            <input type="text" className="eventType" name="eventType"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                       setSearchEventType(event.target.value)}/>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <h3>Personenanzahl:</h3>
+                        <div>
+                            <label htmlFor="zehn">Max. Personenanzahl</label>
+                            <input type="number" className="maxCapacity" name="maxCapacity"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                       setCapacity(event.target.value)}/>
+                        </div>
+                    </fieldset>
+                    <Button>Suchen</Button>
+                </form>
             </section>
             <section>
-                {mapLocations()}
+                {locationCardsCity}
+                {locationCardsSize}
+                {locationCardsPrice}
+                {locationCardsEventType}
+                {locationCardsCapacity}
             </section>
         </section>
 
-)
+    )
 }
