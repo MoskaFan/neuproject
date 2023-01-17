fc9e9d86956b350b732c8de03c1040c4f90cf6a8
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-17 12:12:27 +0100

    Add editLocations

--- backend/src/main/java/de/neuefische/backend/controller/OwnerController.java
+++ backend/src/main/java/de/neuefische/backend/controller/OwnerController.java
@@ -1,4 +1,5 @@
 package de.neuefische.backend.controller;
+
 import de.neuefische.backend.modelle.LocationDTO;
 import de.neuefische.backend.modelle.Owner;
 import de.neuefische.backend.modelle.OwnerDTO;
@@ -10,54 +11,68 @@ import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @RestController
-@RequestMapping ("/api/owners/")
+@RequestMapping("/api/owners/")
 public class OwnerController {
     private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
+
     @PostMapping()
-    public Owner addOwner(@RequestBody OwnerDTO ownerDTO){
+    public Owner addOwner(@RequestBody OwnerDTO ownerDTO) {
         return ownerService.addOwner(ownerDTO);
     }
+
     @PostMapping("/login")
-    public String login(){
+    public String login() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
+
     @GetMapping("/{username}")
-    public Owner getOwnerByUsername(@PathVariable String username){
+    public Owner getOwnerByUsername(@PathVariable String username) {
         return ownerService.getOwnerByUsername(username);
     }
+
     @PutMapping("/login/me/{ownerId}")
-    public Owner addLocation(@PathVariable String ownerId, @RequestBody LocationDTO locationDTO){
+    public Owner addLocation(@PathVariable String ownerId, @RequestBody LocationDTO locationDTO) {
         return ownerService.addLocation(ownerId, locationDTO);
     }
+
     @PutMapping("/{ownerId}")
-    public Owner updateOwner(@PathVariable String ownerId, @RequestBody OwnerDTO owner){
+    public Owner updateOwner(@PathVariable String ownerId, @RequestBody OwnerDTO owner) {
         Owner editedOwner = new Owner(ownerId, owner.username(),
                 owner.email(), owner.password(), owner.locations());
         return ownerService.updateOwner(ownerId, editedOwner);
     }
+
     @GetMapping("/login/me/")
 
-        public String helloMe(Principal principal){
-            if(principal != null){
-                return principal.getName();
-            }
-            return "anonymousUser";
+    public String helloMe(Principal principal) {
+        if (principal != null) {
+            return principal.getName();
         }
+        return "anonymousUser";
+    }
 
     @PostMapping("/logout")
-    public String logout(HttpSession httpSession){
+    public String logout(HttpSession httpSession) {
         httpSession.invalidate();
         SecurityContextHolder.clearContext();
         return "anonymousUser";
     }
+
     @GetMapping("/login/")
-        public String getOwnerIdByUsername(Principal principal){
-            return ownerService.getOwnerIdByUsername(principal);
-        }
+    public String getOwnerIdByUsername(Principal principal) {
+        return ownerService.getOwnerIdByUsername(principal);
     }
+
+    @PutMapping("/locations/{ownerId}/{locationId}")
+    public Owner editLocation(@PathVariable String ownerId,
+                                    @PathVariable String locationId, @RequestBody LocationDTO locationDTO) {
+
+    return ownerService.editLocation(ownerId, locationId, locationDTO);
+    }
+}
 
 
--- backend/src/main/java/de/neuefische/backend/repository/LocationRepository.java
+++ backend/src/main/java/de/neuefische/backend/repository/LocationRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface LocationRepository extends MongoRepository<Location, String> {
 
 
-
+    void findAndModify(String locationId, Location location);
 }
