4fd94c32f77f6253da07b7a9d2a01764b167144f
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2023-01-20 13:16:44 +0100

    Add test wenn_positive_then_location_with_id_140_wil_be_got()

--- backend/src/test/java/de/neuefische/backend/controller/LocationControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/LocationControllerTest.java
@@ -1,5 +1,7 @@
 package de.neuefische.backend.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.backend.modelle.Address;
+import de.neuefische.backend.modelle.Location;
 import de.neuefische.backend.repository.LocationRepository;
 import de.neuefische.backend.service.IDGeneratorService;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,11 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,6 +45,41 @@ class LocationControllerTest {
                          """));
 
 
+
+    }
+    @Test
+    @DirtiesContext
+    @WithMockUser("StandardUser")
+    void wenn_positive_then_location_with_id_140_wil_be_got() throws Exception {
+        Location location = new Location("140", "name",
+                "image", "description", "website", new BigDecimal("120"),
+                20, "Hochzeit", 50,
+                new Address("Deutschland", "Hamburg", "00000", "Test Street",
+                        "12"), null, null);
+
+        locationRepository.save(location);
+        mockMvc.perform(get("/api/locations/140").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                         {"id":"140",
+                                "name": "name",
+                                "image": "image",
+                                "description": "description",
+                                "website": "website",
+                                "pricePerPerson":"120",
+                                "size": 20,
+                                "eventType":"Hochzeit",
+                                "maxCapacity": 50,
+                                "address": {
+                                "country": "Deutschland",
+                                "city": "Hamburg",
+                                "zipCode": "00000",
+                                "street": "Test Street",
+                                "houseNumber": "12"},
+                         "startDate": null,
+                         "endDate": null}
+
+                        """));
 
     }
 }
